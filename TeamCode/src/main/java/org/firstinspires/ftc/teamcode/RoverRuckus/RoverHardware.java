05c46925f5411c9fe0890a8c9cc53bf04da1b700
Teagan <tbrohman@gmail.com>
2019-03-04 19:57:41 -0600

    kk updated timers for tele-op

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/RoverHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/RoverHardware.java
@@ -96,8 +96,8 @@ public class RoverHardware{
     public final double DILBERT_UP = 0.7;
     public final double SORTER_DOWN = 0.38;
     public final double SORTER_UP = 1;
-    public final double DOOR_UP = .7;
-    public final double DOOR_DOWN = .42;
+    public final double DOOR_UP = 0;
+    public final double DOOR_DOWN = .35;
 
 
     public boolean collisionFlag = false;
